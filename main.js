0e9278b530ec53becb94885eab70c2005d661772
nooralam15 <55167857+nooralam15@users.noreply.github.com>
2020-11-26 10:00:16 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 // nickname generator prgram 
 
 //html variables 
-let firstName = document.getElementById("first").value
-let secondName = document.getElementById("second").value
+let randButton = document.getElementById("random").addEventListener("click", randomNickname)
+let allNameButton = document.getElementById("all").addEventListener("click", allNicknames)
+let containerEl = document.getElementById("container")
 
 //global variable
-let nicknames 
+let nickNames 
 
 //fetch data from text fle
 fetch("nickname.txt").then(convertData).then(processData)
@@ -16,5 +17,28 @@ function convertData(rawData) {
 }
 
 function processData(stringData) {
-    colors = stringData.split(/\r?\n/)
+    //create the array
+    nickNames = stringData.split(/\r?\n/)
+}
+
+//generate a random nickname
+function randomNickname() {
+    let firstName = document.getElementById("first").value
+    let lastName = document.getElementById("last").value
+    let divStr= "" 
+    let randomIndex = Math.floor(Math.random() * nickNames.length)
+    //console.log(randomIndex)
+    divStr += `<div>${firstName} the ${nickNames[randomIndex]} ${lastName}</div>`
+    containerEl.innerHTML = divStr
+}
+
+//generate all the nicknames
+function allNicknames() {
+    let firstName = document.getElementById("first").value
+    let lastName = document.getElementById("last").value
+    let divStr = ""
+    for (let i=0; i < nickNames.length; i++) {
+        divStr += `<div>${firstName} the ${nickNames[i]} ${lastName}</div>`
+    }
+    containerEl.innerHTML = divStr
 }
